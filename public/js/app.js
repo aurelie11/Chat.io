33f477d46b7c8eb280736bf3707d35b9cf1946ac
Aurelie Lecoq <aurelie.lecoq1992@gmail.com>
2017-09-15 16:40:50 +0200

    corrigé

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,35 @@
 (function(){
+  // creation de la socket client
   var socket = io();
-  console.log('hello')
+  //creation du callback du submit de message valider
   $("#chat").submit(function(e){
+    //annule le comportement par defaut de la validation de formulaire
     e.preventDefault()
-    var message = $("#chat-text")
-
-    socket.emit('message-send',
-      message.val())
-
+    var message = $('#chat-text')
+    // envoi du message au serveur et clear de l'imput
+    socket.emit('message-send', message.val())
     message.val('');
+  });
+  // attente de l'evenement message du serveur
+  socket.on('message', function(msg){
+    //affichage du pseudo + message sur la page
+    $("#chat-body").append('<li>'+ msg.user +': '+msg.message+'</li>')
+  });
+  // attente de l'event service message qui previen d'une connection utilisateur
+  socket.on('service-message', function(user){
+    $("#chat-body").append('<li>'+user+' is connected </li>')
   })
-
-  socket.on('message', function(message){
-    $('#message').append('<li>'+message+'</li>')
+  //creation du callback de validation de login
+  $("#login").submit(function(e){
+    //annule le comportement par defaut de la validation du formulaire
+    e.preventDefault();
+    var username = $('#username')
+    //envoi du pseudo au serveur et clear de l'input
+    socket.emit('login', username.val())
+    username.val('')
+    //cache le formulaire de connection et affiche le formulaire de message a l'aide d'une classe
+    $('#chat').removeClass()
+    $('#login').addClass('hidden')
   })
 
 })()
-
--- server.js
+++ server.js
@@ -1,24 +1,37 @@
+/*
+  script de configuration serveur
+*/
+
+//chargement des modules nodes dans des variables
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var socket = require('socket.io');
+//lancement de l'ecoute serveur
 var server = app.listen(8000);
 var io = socket(server);
 var ent = require('ent');
-
-
-
+//definition d'un dossier accessible pour le navigateur
 app.use(express.static('public'));
-console.log('hello')
 
+//déclaration de l'attente d'un evennement connection
 io.on('connection', function(socket){
-  console.log('User as connected !');
+  //creation d'une variable pour stocker le pseudo
+  var loggedUser;
+  console.log('User is connected !');
   socket.on('disconnect', function(){
-    console.log('User as diconnected !')
+    console.log('User is disconnected !');
+  });
+  // attente de l'evenement login pour recup le login de l'utilisateur
+  socket.on('login', function(user){
+    loggedUser = ent.encode(user);
+    //envoi de la notification de connection aux autres utilisateurs
+    socket.broadcast.emit('service-message', loggedUser)
   })
 
+  //attente de l'evenement message-send depuis les utilisateurs
   socket.on('message-send', function(msg){
-    msg = ent.encode(msg);
-      io.emit('message', msg)
-    })
+    //envoi du message a tout les utilisateurs
+    io.emit('message', {message: ent.encode(msg), user: loggedUser})
+  })
 })
